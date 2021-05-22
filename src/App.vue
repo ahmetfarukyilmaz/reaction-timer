0647788f1509cd2c96faf6c2575af51d912cf1ad
ahmetfarukyilmaz <farukylm34@gmail.com>
2021-05-22 05:04:52 +0300

    add start button,implement timing algorithm

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Test Your Reflexes !</h1>
+  <button @click="start" :disabled="isPlaying">Start</button>
+  <ClickBlock v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ClickBlock from "./components/ClickBlock.vue";
+import Results from "./components/Results.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ClickBlock,Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      showResults: false,
+      score: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 1500 + Math.random() * 3000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+
+    endGame(reactionTime) {
+      this.showResults = true;
+      this.score=reactionTime
+      this.isPlaying=false
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,43 @@
+<template>
+  <p>Reaction Time : {{score}} ms </p>
+  <p>{{rank}}</p>
+</template>
+
+<script>
+export default {
+
+    props:['score'],
+
+    data() {
+        return {
+            rank:null
+        }
+
+    },
+
+    mounted() {
+
+        if(this.score<250)
+        this.rank="INHUMAN"
+        else if (this.score<400)
+        this.rank="PRETTY GOOD"
+        else if (this.score<600)
+        this.rank="You can do better."
+        else
+        this.rank="We can not be good at everything right ?"
+    }
+
+
+
+
+
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
